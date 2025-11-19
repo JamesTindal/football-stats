bfdad21c4ac3f49d58e413c9f3084fbd1b08a0ac
James Tindal <james@tindal.uk>
2025-11-19 23:49:53 +0000

    Initial commit

---
+++ JT.FootballStats.Core/Models/League.cs
@@ -0,0 +1,11 @@
+﻿namespace JT.FootballStats.Core.Models;
+
+public class League
+{
+    public required int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public required List<Team> Teams { get; set; }
+    
+}
---
+++ JT.FootballStats.Core/Models/Team.cs
@@ -0,0 +1,9 @@
+namespace JT.FootballStats.Core.Models;
+
+public class Team
+{
+    public required int Id { get; set; }
+
+    public required string Name { get; set; }
+    
+}
